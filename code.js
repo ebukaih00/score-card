1d4867ced96b856b2d1f05f1654be472801d30b5
Chukwuebuka ihueze <ihuezeebuka5@gmail.com>
2026-04-22 17:29:38 +0100

    refactor score display logic and clean up commented code

--- code.js
+++ code.js
@@ -4,53 +4,35 @@ let players = [
     { name: "Charlie", score: 0, }
 ]
 
-// let scoreEl = document.querySelector(".score-container")
+
+let containerEl = document.querySelector(".score-container")
+
+for (let i=0; i<players.length; i++){
+containerEl.innerHTML += `
+<div>
+<p> ${players[i].name}</p>
+<p id="score-${i}"> ${players[i].score}</p>
+<button onclick = " updatescore(${i}, 1)"> + </button>
+<button onclick = "updatescore(${i}, -1)"> - </button>
+</div>
+`
+}
 
 
-// for(let i=0; i<players.length; i++){
-//     scoreEl.innerHTML += 
-//     `
-//     <div>
-//     <p>${players[i].name}</p>
-//      <p id="score-${i}">${players[i].score}</p>
-//      <button onclick="updateScore(${i},1)"> +</button>
-//      <button onclick="updateScore(${i},-1)"> -</button>
-//      </div>
-//     `
-// }
-
-// function updateScore(index, change) {
-
-//     // 1. update the score in the array
-//     players[index].score += change
-
-//     // 2. find the right score element on screen
-//     let scoreEl = document.getElementById("score-" + index)
-
-//     // 3. update what it shows 
-//     scoreEl.innerText = players[index].score
-
-//     // 4. change colour based on score value
-//     if(players[index].score > 0) {
-//         scoreEl.style.color = "green";
-//     } else if (players[index].score < 0 ) {
-//         scoreEl.style.color = "red"; 
-//     } 
-//     else {
-//       scoreEl.style.color = "black";
-//     }
-// }
+function updatescore(index, change){
+    players[index].score += change
+    let scoreEl = document.getElementById("score-" + index) 
+    scoreEl.innerText = players[index].score
 
 
+if (players[index].score > 0){
+    scoreEl.style.color = "green"
+}
 
+else if (players[index].score < 0){
+    scoreEl.style.color = "red"
+}
 
+else (scoreEl.style.color = "black")
+}
 
-
-// scoreEl.innerHTML += `
-//     <div>
-//         <p>${players[i].name}</p>
-//         <p id="score-${i}">${players[i].score}</p>
-//         <button onclick="updateScore(${i}, 1)">+</button>
-//         <button onclick="updateScore(${i}, -1)">-</button>
-//     </div>
-// `
