4531b9f20583af97cd6415ac52163f13471e87e3
Chukwuebuka ihueze <ihuezeebuka5@gmail.com>
2026-04-23 08:28:49 +0100

    third attempt

--- code.js
+++ code.js
@@ -4,35 +4,33 @@ let players = [
     { name: "Charlie", score: 0, }
 ]
 
-
-let containerEl = document.querySelector(".score-container")
+let contEl = document.querySelector(".score-container")
 
 for (let i=0; i<players.length; i++){
-containerEl.innerHTML += `
-<div>
-<p> ${players[i].name}</p>
-<p id="score-${i}"> ${players[i].score}</p>
-<button onclick = " updatescore(${i}, 1)"> + </button>
-<button onclick = "updatescore(${i}, -1)"> - </button>
-</div>
-`
+    contEl.innerHTML += `
+    <div>
+   <p > ${players[i].name}</p> 
+   <p id= "score-${i}"> ${players[i].score}</p> 
+    <button onclick= "updateScore(${i}, 1)"> + </button>
+     <button onclick= "updateScore(${i}, -1)"> + </button>
+    </div>
+    `
 }
 
 
-function updatescore(index, change){
-    players[index].score += change
-    let scoreEl = document.getElementById("score-" + index) 
+
+function updateScore(index, change){
+    players[index].score +=change
+    let scoreEl = document.getElementById("score-" + index)
     scoreEl.innerText = players[index].score
 
+    if (players[index].score<0){
+        scoreEl.style.color = "red"
+    }
 
-if (players[index].score > 0){
-    scoreEl.style.color = "green"
+    else if (players[index].score>0)  {
+        scoreEl.style.color = "green"
+    }
+
+    else (scoreEl.style.color = "black")
 }
-
-else if (players[index].score < 0){
-    scoreEl.style.color = "red"
-}
-
-else (scoreEl.style.color = "black")
-}
-
